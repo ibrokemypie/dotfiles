1fa359c271ba8e1b3a2e68e6544e1da9147e82df
ibrokemypie <ibrokemypie@bastardi.net>
2017-06-01 09:46:50 +1000

    some changes

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,63 @@
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/The Bitter End.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Alpha Omega.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/These Colours Don't Run.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Daybreak.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Truth, Be Told.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Even If You Win, You're Still a Rat.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Outsider Heart.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Behind The Throne.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Devil's Island.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Feather Of Lead.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Unbeliever.ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Black Blood (Bonus Track).ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Cracks In The Earth (Bonus Track).ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Blood Bank (Bonus Track).ogg
+/Users/Conor/Desktop/music/complete/Architects - 2013 - Daybreaker/Of Dust And Nations (Bonus Track).ogg
+/Users/Conor/Desktop/music/complete/Dissonants/01 I Am.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/02 Perspectives.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/03 Colourblind.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/04 New Romantics.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/05 Glasshouse.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/06 Division Symbols.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/07 Stillwater.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/08 Momentary.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/09 Motion Sickness.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/10 Degrees of Separation.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/11 Grey Havens.ogg
+/Users/Conor/Desktop/music/complete/Dissonants/12 Bloodlines.ogg
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/01 - We Are All.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/02 - Your Disease.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/03 - Outcasts (Ft. Andy Leo of Crown The Empire).mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/04 - A Disasterpiece.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/05 - The Reckoning (Ft. Chris Roetter of Like Moths to Flames).mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/06 - High & Low (Ft. Tyler Carter of Issues).mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/07 - The Arctic.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/08 - A.I..mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/09 - Betrayed.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/10 - Sidney.mp3
+/Users/Conor/Desktop/music/complete/Palisades - Outcast/11 - Scarred.mp3
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/01 - Aggression.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/02 - Cold Heart (Warm Blood).flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/03 - Better Chemicals.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/04 - Fall.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/05 - Let Down.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/06 - Dark.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/07 - Through Hell.flac
+/Users/Conor/Desktop/music/complete/palisades - palisades (2017) {rise 350-2 cd} [flac]/08 - Memories.flac
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/01 Hard Times.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/02 Rose-Colored Boy.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/03 Told You So.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/04 Forgiveness.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/05 Fake Happy.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/06 26.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/07 Pool.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/08 Grudges.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/09 Caught in the Middle.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/10 Idle Worship.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/11 No Friend.ogg
+/Users/Conor/Desktop/music/complete/Paramore - After Laughter (2017) [WEB OPUS]/12 Tell Me How.ogg
+/Users/Conor/Desktop/music/complete/[non-album tracks]/00 Air Catcher (Studio Version).mp3
+/Users/Conor/Desktop/music/complete/[non-album tracks]/00 Migraine (Toy Piano Edition).mp3
+/Users/Conor/Desktop/music/complete/[non-album tracks]/00 Save (Studio Edition).mp3
+/Users/Conor/Desktop/music/complete/[non-album tracks]/00 Two.mp3
+/Users/Conor/Desktop/music/complete/[non-album tracks]/Drown.ogg

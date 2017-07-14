2b11d9495bb7e01a53939f969138fecc7af84dfa
ibrokemypie <ibrokemypie@bastardi.net>
2017-07-14 19:45:49 +1000

    Switched back to arch without systemd

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-exec xautolock -detectsleep -time 15 -locker "~/i3lock-fancy/lock -- maim" \
+exec xautolock -detectsleep -time 15 -locker "i3lock-next" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"

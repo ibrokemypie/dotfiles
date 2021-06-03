ae57db67f9554db3a6f169fc4cf91ce8be357ba2
ibrokemypie <ibrokemypie@outlook.com>
2021-06-03 20:49:23 +1000

    Use swaymsg subscribe for selected window

--- .config/waybar/window.sh
+++ .config/waybar/window.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-while true; do
-    window=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused==true) | {name} | to_entries | .[] | .value ')
-    if [[ "$window" == "1" ]]; then
-        window=""
-    fi
-    printf '%.30s\n' "$window"
-    sleep 0.2
-done
+swaymsg -t subscribe -m '["window"]' | jq -r --unbuffered '.container.name | .[0:30]'

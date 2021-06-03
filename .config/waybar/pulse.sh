a40be3476f7537e1decfb8d76dcdfd650d62b8a8
ibrokemypie <ibrokemypie@outlook.com>
2021-06-03 21:37:26 +1000

    Subscribe to pactl events instead of checking every few ms

--- .config/waybar/pulse.sh
+++ .config/waybar/pulse.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+function print_output() {
+    cur_sink_name=$(pactl info | grep "Default Sink" | cut -d" " -f3)
+    sinks=$(pactl list sinks)
+    volume=$(echo "$sinks" | grep -A 15 -i "$cur_sink_name" | grep -i "volume" | awk -F : '{print $3; exit}' | grep -o -P '.{0,3}%' | sed 's/.$//' | tr -d ' ' )
+    muted=$(echo "$sinks" | grep -A 15 -i "$cur_sink_name" | awk '/Mute/ {print $2; exit}')
+
+    if [[ ${cur_sink_name} == "alsa_output.pci-0000_00_1b.0.analog-stereo" ]]; then
+        name="analog"
+    elif [[ ${cur_sink_name} == "alsa_output.pci-0000_00_1b.0.iec958-stereo" ]]; then
+        name="digital"
+    elif [[ ${cur_sink_name} == "bluez_output.CC_98_8B_80_7B_CC.a2dp-sink" ]];then
+        name="bluetooth"
+    else
+        name=${cur_sink_name}
+    fi
+    
+    if [ "$muted" == "yes" ]; then
+        class="muted"
+        alt="muted"
+    else
+        class="playing"
+        alt="playing"
+    fi
+    output="on $name"
+    printf '{"text": "%s", "class": "%s", "percentage": %d, "alt": "%s"}\n' "$output" "$class" "$volume" "$alt"
+}
+
 function main() {
     action=$1
     if [ "${action}" == "up" ]; then
@@ -9,32 +36,9 @@ function main() {
     elif [ "${action}" == "mute" ]; then
         pactl set-sink-mute @DEFAULT_SINK@ toggle
     else
-        while true; do
-            cur_sink_name=$(pactl info | grep "Default Sink" | cut -d" " -f3)
-            sinks=$(pactl list sinks)
-            volume=$(echo "$sinks" | grep -A 15 -i "$cur_sink_name" | grep -i "volume" | awk -F : '{print $3; exit}' | grep -o -P '.{0,3}%' | sed 's/.$//' | tr -d ' ' )
-            muted=$(echo "$sinks" | grep -A 15 -i "$cur_sink_name" | awk '/Mute/ {print $2; exit}')
-
-            if [[ ${cur_sink_name} == "alsa_output.pci-0000_00_1b.0.analog-stereo" ]]; then
-                name="analog"
-            elif [[ ${cur_sink_name} == "alsa_output.pci-0000_00_1b.0.iec958-stereo" ]]; then
-                name="digital"
-            elif [[ ${cur_sink_name} == "bluez_output.CC_98_8B_80_7B_CC.a2dp-sink" ]];then
-                name="bluetooth"
-            else
-                name=${cur_sink_name}
-            fi
-            
-            if [ "$muted" == "yes" ]; then
-                class="muted"
-                alt="muted"
-            else
-                class="playing"
-                alt="playing"
-            fi
-            output="on $name"
-            printf '{"text": "%s", "class": "%s", "percentage": %d, "alt": "%s"}\n' "$output" "$class" "$volume" "$alt"
-            sleep 0.2
+        print_output
+        pactl subscribe | grep --line-buffered change | while read -r; do
+            print_output
         done
     fi
 }

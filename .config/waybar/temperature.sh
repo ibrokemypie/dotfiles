ee6ab411329fc40a02f101c4469cfcbc67e4be2b
ibrokemypie <ibrokemypie@outlook.com>
2021-06-05 23:42:35 +1000

    Update waybar config

--- .config/waybar/temperature.sh
+++ .config/waybar/temperature.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 
 input=$1
-interval=$2
-while true; do
-    temp_long=$(cat "$input")
-    temp=$(expr "$temp_long" / 1000)
-    if (( temp  < 20 )); then
-        class="default"
-    elif (( temp < 45)); then
-        class="good"
-    elif (( temp < 60 )); then
-        class="idle"
-    elif (( temp < 80)); then
-        class="info"
-    else
-        class="warning"
-    fi
-    printf '{"text": "%s", "class": "%s" }\n' "$temp" "$class" 
-    sleep "$interval"
-done
+temp_long=$(cat "$input")
+temp=$(expr "$temp_long" / 1000)
+if (( temp  < 20 )); then
+    class="idle"
+elif (( temp < 45)); then
+    class="default"
+elif (( temp < 60 )); then
+    class="good"
+elif (( temp < 80)); then
+    class="info"
+else
+    class="warning"
+fi
+printf '{"text": "%s", "class": "%s" }\n' "$temp" "$class" 

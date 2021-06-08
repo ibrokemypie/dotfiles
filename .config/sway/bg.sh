4edfdaa0a1b39b0ec2182db1a506dce86e6074cd
ibrokemypie <ibrokemypie@outlook.com>
2021-06-08 15:30:49 +1000

    Use current wallpaper as lockscreen image

--- .config/sway/bg.sh
+++ .config/sway/bg.sh
@@ -5,7 +5,8 @@ while true; do
 	[ -n "$SWAYSOCK" ]
 	bgn="$(ls $bgdir | shuf -n 1)"
 	img="$bgdir/$bgn"
-	#swaymsg -s $SWAYSOCK output "*" bg "$img" fill
-	swaybg -i "$img" -m fill -o "*"
+	swaymsg -s "$SWAYSOCK" output "*" bg "$img" fill
+	rm "$HOME/.config/swaylock/bg"
+	ln -s "$img" "$HOME/.config/swaylock/bg"
 	sleep 60m
 done

5a9047ece347c9cd9d6cfa30e8a67107f0a28699
ibrokemypie <ibrokemypie@outlook.com>
2021-10-31 19:03:33 +1100

    waybar config updates

--- .config/yadm/alt/.config/waybar##os.Linux/temperature.sh
+++ .config/yadm/alt/.config/waybar##os.Linux/temperature.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
 input=$1
-temp_long=$(cat "$input")
-temp=$(("$temp_long" / 1000))
-if (( temp  < 20 )); then
-    class="idle"
-elif (( temp < 45)); then
-    class="default"
-elif (( temp < 60 )); then
-    class="good"
-elif (( temp < 80)); then
-    class="info"
-else
-    class="warning"
+if test -f "$input"; then
+	temp_long=$(cat "$input")
+	temp=$(("$temp_long" / 1000))
+	if (( temp  < 20 )); then
+	    class="idle"
+	elif (( temp < 45)); then
+	    class="default"
+	elif (( temp < 60 )); then
+	    class="good"
+	elif (( temp < 80)); then
+	    class="info"
+	else
+	    class="warning"
+	fi
+	printf '{"text": "%s", "class": "%s", "alt": "%s" }\n' "$temp" "$class" "$class"
 fi
-printf '{"text": "%s", "class": "%s", "alt": "%s" }\n' "$temp" "$class" "$class"
